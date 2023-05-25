95eee68c3bf1ea2648cfcb02cb5d74fdb8941a66
Nikita <nik.qaz321@gmail.com>
2023-05-26 01:22:12 +0300

    3.2

--- src/L1List.java
+++ src/L1List.java
@@ -1,4 +1,4 @@
-public class L1List<T> {
+public class L1List<T>{
     private Node<T> first, last; //Ссылки на первый и последний узлы.
     private int size;
     public L1List(){
@@ -15,14 +15,45 @@ public class L1List<T> {
         last = node;
         size++;
     }
+
+    //Получить номер элемента.
     public int find(Node node){
         int index = 0;
+        Node temp = first;
+        while (temp != node){
+            temp = temp.next;
+            index++;
+        }
+        return index;
+    }
 
-      return 0;
-    };
+    //Вставка элемента.
     public void insert(Node node, int location){
-        return;}
-    public void remove(int location){
+        Node r = getNode(location + 1);
+        Node l = getNode(location - 1);
+        l.next = node;
+        node.next = r;
+        size++;
 
-    };
+    }
+
+    //Удаление элемента.
+    public void remove(int location){
+        Node right = getNode(location + 1);
+        Node left = getNode(location - 1);
+        left.next = right;
+        size--;
+    }
+    public int getSize(){
+        return size;
+    }
+
+    //Получить элемент по местоположению
+    public Node getNode(int location){
+        Node temp = first;
+        for(int i = 0; i < location; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         list.append(new Node<>(4));
         list.append(new Node<>(5));
         list.remove(3);
-        list.printList();
+        for (int i = 0; i < list.getSize(); i++){
+            System.out.print(list.getNode(i).value + " ");
+        }
     }
 }
